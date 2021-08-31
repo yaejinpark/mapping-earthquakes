c317b255827d8b30c385bebf4ecf7ad37d5e07e7
yaejinpark <yaejinpark@Yae-Jins-MacBook-Pro.local>
2021-08-30 23:05:45 -0700

    geoJSON example done with markers and popups

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,28 +1,12 @@
-// Add console.log to check to see if our code is working.
-console.log("working");
-
 // Create the map object with a center and zoom level.
 let map = L.map("mapid", {
   center: [
-    37.6213, -122.3790
+    37.5, -122.5
   ],
-  zoom: 5
+  zoom: 10
 });
 
-// Add circle to Map
-// L.circle([34.0522, -118.2437], {
-//    radius: 300,
-//    color:'black',
-//    fillColor:'#FFFFA1'
-// }).addTo(map);
-
 let cityData = cities;
-
-// Coordinates for each point to be used in the line (single).
-// let line = [
-//   [33.9416, -118.4085],
-//   [37.6213, -122.3790]
-// ];
 
 // Coordinates for each point to be used in the polyline.
 let line = [
@@ -31,6 +15,42 @@ let line = [
   [40.7899, -111.9791],
   [47.4502, -122.3088]
 ];
+
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
+
+// Grabbing our GeoJSON data.
+L.geoJson(sanFranAirport, {
+    // We turn each feature into a marker on the map.
+    pointToLayer: function(feature, latlng) {
+      console.log(feature);
+      return L.marker(latlng)
+      // .bindPopup("<h2>" + feature.properties.name + "</h2> <hr> <h3>" + feature.properties.city + ", " + feature.properties.country);
+    },
+    // Allow markup's popup on all data
+    onEachFeature: function(feature, layer) {
+    	console.log(layer);
+    	layer.bindPopup("<h2>Airport Code: " + feature.properties.faa + "</h2> <hr> <h3>Airport name: " + feature.properties.name + "</h3>" );
+    }
+
+  }).addTo(map);
 
 // Create a polyline using the line coordinates and make the line red.
 L.polyline(line, {
