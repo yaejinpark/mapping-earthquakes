e7e0b0ffa5d9722999a3866f9de171341b3d9cec
yaejinpark <yaejinpark@Yae-Jins-MacBook-Pro.local>
2021-08-30 23:50:05 -0700

    added toronto route line style and popups

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,5 +1,5 @@
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
 	accessToken: API_KEY
@@ -20,7 +20,7 @@ let baseMaps = {
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-    center: [30, 30],
+    center: [44.0, -80.0],
     zoom: 2,
     layers: [streets]
 })
@@ -31,16 +31,38 @@ L.control.layers(baseMaps).addTo(map);
 // Accessing the airport GeoJSON URL
 let airportData = "https://raw.githubusercontent.com/yaejinpark/mapping-earthquakes/Mapping_GeoJSON_Points/majorAirports.json";
 
+// Accessing Toronto Routes Data GeoJSON URL
+let torontoData = "https://raw.githubusercontent.com/yaejinpark/mapping-earthquakes/Mapping_GeoJSON_Linestrings/torontoRoutes.json";
+
+// Create a style for the lines.
+let myStyle = {
+    color: "#ffffa1",
+    weight: 2
+}
+
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
-  // Adding popup information on all airport locations
+d3.json(torontoData).then(function(data) {
+    console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
   L.geoJson(data, {
+  	style: myStyle,
   	onEachFeature: function(feature, layer) {
   		console.log(layer);
-     	layer.bindPopup("<h2>Airport Code: " + feature.properties.faa + "</h2> <hr> <h3>Airport name: " + feature.properties.name + "</h3>" );
+     	layer.bindPopup("<h3>Airline: " + feature.properties.airline+ "</h3 <hr> <h3>Destination: " + feature.properties.dst + "</h3>" );
   	}
   }).addTo(map);
 });
+
+// // Grabbing our GeoJSON data.
+// d3.json(airportData).then(function(data) {
+//   // Adding popup information on all airport locations
+//   L.geoJson(data, {
+//   	onEachFeature: function(feature, layer) {
+//   		console.log(layer);
+//      	layer.bindPopup("<h2>Airport Code: " + feature.properties.faa + "</h2> <hr> <h3>Airport name: " + feature.properties.name + "</h3>" );
+//   	}
+//   }).addTo(map);
+// });
 
 // Then we add our 'graymap' tile layer to the map.
 // streets.addTo(map);
